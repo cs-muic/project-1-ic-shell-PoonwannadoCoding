9e61ec7ae077d0b08163cea33059f7feb5e8fdcc
worawit <worawit.pen@student.mahidol.edu>
2022-09-23 21:57:34 +0700

    complete exit and fix the segmentation fault

--- icsh.c
+++ icsh.c
@@ -4,28 +4,38 @@
  */
 
 #include "stdio.h"
-#include <stdbool.h>
+#include <stdlib.h>
 #include <string.h>
+
 
 #define MAX_CMD_BUFFER 255
 
 int main() {
     char buffer[MAX_CMD_BUFFER];
-	bool isStart = true;
+	
 	printf("Starting IC shell\n");
-    while (isStart) {
+    while (1) {
         printf("icsh $ ");
         fgets(buffer, 255, stdin);
 		
 		char *token = strtok(buffer, " "); // split the input
 		
 		if (strcmp(token, "echo") == 0){
-			token = strtok(NULL, " ");
+			token = strtok(NULL, " ");	
 			while(token != NULL){
 				printf("%s ", token);
 				token = strtok(NULL, " ");
 			}
+			
 		}
+
+		else if (strcmp(token, "exit") == 0){
+		int result = atoi(strtok(NULL, " "));
+			
+			return (int) result;
+		}
+		
+
         
     }
 }
