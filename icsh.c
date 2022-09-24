dd765ce58f78d2d817e4a4eb11dea330e2493586
worawit <worawit.pen@student.mahidol.edu>
2022-09-25 03:02:39 +0700

    Split the main func and create 2 helper func also having a bug with git add *

--- icsh.c
+++ icsh.c
@@ -6,38 +6,97 @@
 #include "stdio.h"
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
+int command(char **);
+char ** splitToken(char *);
 
 
 #define MAX_CMD_BUFFER 255
+#define MAX_STRING 255
+#define MAX_TOKEN 100
 
 int main() {
     char buffer[MAX_CMD_BUFFER];
-	
+	char** rec;
+	char * history = (char *)malloc(MAX_TOKEN);
+	int active = 1;
+	int index = 0;
+
 	printf("Starting IC shell\n");
-    while (1) {
+    while (active) {
+
         printf("icsh $ ");
         fgets(buffer, 255, stdin);
-		
-		char *token = strtok(buffer, " "); // split the input
-		
-		if (strcmp(token, "echo") == 0){
-			token = strtok(NULL, " ");	
-			while(token != NULL){
-				printf("%s ", token);
-				token = strtok(NULL, " ");
-			}
+		rec = splitToken(buffer);
+			
+
+		if (strcmp(rec[0], "!!\n") != 0){
+			strcpy(history, buffer);
 			
 		}
 
-		else if (strcmp(token, "exit") == 0){
-		int result = atoi(strtok(NULL, " "));
-			
-			return (int) result;
+		if(strcmp(rec[0], "exit") == 0){
+			printf("Bye \n");
+			return (int) rec[1];
 		}
-		
 
-        
-    }
+		else if(strcmp(rec[0], "!!\n") == 0 && strcmp(history, "!!\n") != 0){
+
+			printf("!! => %s \n", history);
+			char ** prevTok = splitToken(history);
+			active = command(prevTok);
+		}
+
+
+		else{
+			
+			active = command(rec);
+		}		
+	}
+	return 0;
+}
+
+
+
+char ** splitToken(char * args){
+	
+	int index = 0;
+	char ** tokens = malloc(MAX_TOKEN * sizeof(char *));
+	char * token;
+
+	token = strtok(args, " ");
+	while(token != NULL){
+		tokens[index] = token;
+		index ++;
+
+		if(index >= (MAX_TOKEN - 1)){
+			break;
+		}
+		token = strtok(NULL, " ");
+	}
+	tokens[index] == NULL;
+	return tokens;
+}
+
+
+int command(char ** args){
+
+	if (args[0] == NULL){
+		return 1;
+	}
+	else if ( strcmp(args[0], "echo") == 0){
+		for(int i = 1; args[i] != NULL; i++){
+			printf("%s ", args[i]);
+		}
+		printf("\n");
+		return 1;
+	}
+	else {
+		printf("Un known command\n");
+	}
+	return 1;
+
 }
 
 
